c075e3e10eb87cbb26c0aae4b630633473009614
Kacper Kisielewski <s30670@pjwstk.edu.pl>
2024-03-28 11:51:55 +0100

    added tests for Student and StudentGroup

--- src/StudentGroupTest.java
+++ src/StudentGroupTest.java
@@ -1,18 +1,60 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentGroupTest {
+    private static List<Double> grades = new ArrayList<>();
+    public static Student student = new Student("John", "Doe", "s00000", "john.doe@example.com", "NA", grades);
+
+    @Test
+    void constructorStudent() {
+        List<Student> studentList = Arrays.asList(student);
+        StudentGroup group = new StudentGroup("Test Group", studentList);
+
+        assertEquals(studentList, group.getStudents());
+    }
 
     @Test
     void addStudent() {
+        StudentGroup group = new StudentGroup("Test Group");
+        group.addStudent(student);
+
+        assertTrue(group.getStudents().contains(student));
+    }
+
+    @Test
+    void exceedsLimitAddStudent() {
+        StudentGroup group = new StudentGroup("Test Group");
+        IllegalArgumentException expectedError = new IllegalArgumentException("Number of students in a group cannot exceed 15");
+
+        assertThrowsExactly(expectedError.getClass(), () -> {
+            for (int i = 0; i < 16; i++) {
+                group.addStudent(new Student("John" + i, "Doe", "s00000", "nothing@example.com", "NA"));
+            }
+        });
+    }
+
+    @Test
+    void studentAlreadyAddedAddStudent() {
+        StudentGroup group = new StudentGroup("Test Group");
+        IllegalArgumentException expectedError = new IllegalArgumentException("Student was already added to the group");
+
+        assertThrowsExactly(expectedError.getClass(), () -> {
+            group.addStudent(student);
+            group.addStudent(student);
+        });
     }
 
     @Test
     void removeStudent() {
-    }
+        StudentGroup group = new StudentGroup("Test Group");
+        group.addStudent(student);
+        group.removeStudent(student);
 
-    @Test
-    void getStudents() {
+        assertFalse(group.getStudents().contains(student));
     }
 }
--- src/StudentTest.java
+++ src/StudentTest.java
@@ -17,6 +17,13 @@ class StudentTest {
     }
 
     @Test
-    void addToGroup() {
+    void errorExceedsLimitCalculateGradesAverage() {
+        IllegalArgumentException expectedError = new IllegalArgumentException("Number of grades cannot exceed 20");
+        List<Double> grades = new ArrayList<>();
+        for (int i = 1; i <= 21; i++)
+            grades.add(0.0);
+        Student student = new Student("John", "Doe", "s00000", "john.doe@example.com", "NA", grades);
+
+        assertThrowsExactly(expectedError.getClass(), () -> student.calculateGradesAverage());
     }
 }
